f4e4bb4f94dd82908212d3203f415b5436b6ef9c
gaoyangyang <gaoyangyang@sinovatech.com>
2020-01-20 15:31:14 +0800

    代码整理

--- src/main/java/com/lean/util/RemoteLoad.java
+++ src/main/java/com/lean/util/RemoteLoad.java
@@ -13,37 +13,36 @@ import com.google.common.base.Joiner;
 @Component
 public class RemoteLoad {
 
-    @Autowired
-    private RestTemplate rest;
+  @Autowired
+  private RestTemplate rest;
 
-    public <T> T postRestObject(String serviceName, String url, Class<T> clazz, Map<String, Object> pars) {
-        return postRestObjectRetry(serviceName, url, clazz, pars, 0);
-    }
+  public <T> T postRestObject(String serviceName, String url, Class<T> clazz, Map<String, Object> pars) {
+    return postRestObjectRetry(serviceName, url, clazz, pars, 0);
+  }
 
-    private <T> T postRestObjectRetry(String serviceName, String url, Class<T> clazz, Map<String, Object> pars,
-            Integer retryTime) {
-        try {
-            T data = rest.postForObject(Joiner.on("").join("http://", serviceName, url), convertPar(pars), clazz);
-            return data;
-        } catch (Exception e) {
-            // TODO: handle exception
-            if (retryTime < 3) {
-                return postRestObjectRetry(serviceName, url, clazz, pars, retryTime + 1);
-            }
-            e.printStackTrace();
-            return null;
-        }
+  private <T> T postRestObjectRetry(String serviceName, String url, Class<T> clazz, Map<String, Object> pars, Integer retryTime) {
+    try {
+      T data = rest.postForObject(Joiner.on("").join("http://", serviceName, url), convertPar(pars), clazz);
+      return data;
+    } catch (Exception e) {
+      // TODO: handle exception
+      if (retryTime < 3) {
+        return postRestObjectRetry(serviceName, url, clazz, pars, retryTime + 1);
+      }
+      e.printStackTrace();
+      return null;
     }
+  }
 
-    // 把map放到MultiValueMap中 用来传值
-    private MultiValueMap<String, Object> convertPar(Map<String, Object> par) {
-        MultiValueMap<String, Object> result = new LinkedMultiValueMap<String, Object>();
-        if (par == null) {
-            return result;
-        }
-        for (String key : par.keySet()) {
-            result.add(key, par.get(key));
-        }
-        return result;
+  // 把map放到MultiValueMap中 用来传值
+  private MultiValueMap<String, Object> convertPar(Map<String, Object> par) {
+    MultiValueMap<String, Object> result = new LinkedMultiValueMap<String, Object>();
+    if (par == null) {
+      return result;
     }
+    for (String key : par.keySet()) {
+      result.add(key, par.get(key));
+    }
+    return result;
+  }
 }
--- src/main/java/com/lean/web/UserController.java
+++ src/main/java/com/lean/web/UserController.java
@@ -13,25 +13,25 @@ import com.lean.util.ApiUtils;
 import com.lean.util.RemoteLoad;
 
 @RestController
-public class UserController extends RemoteLoad{
+public class UserController extends RemoteLoad {
 
-    @Value("${service.url.order}")
-    private String serviceOrderUrl;
-    
-    @RequestMapping(value = "/testRestTemplate")
-    public ResponseModel testRestTemplate (HttpServletRequest request) {
-        
-        HashMap<String, Object> par = new HashMap<String, Object>();
-        //request值放到HashMap中
-        ApiUtils.setRequestPar(request,par);
-        //postRestObject参数说明
-        //【0】服务名称 serviceOrderUrl
-        //【1】接口地址 /order/getUserOrderInfo
-        //【2】返回类型 ResponseModel.class
-        //【3】参数类型 par
-          ResponseModel res = postRestObject(serviceOrderUrl, "/order/getUserOrderInfo", ResponseModel.class, par);
-        return res;
-        
-    }
-    
+  @Value("${service.url.order}")
+  private String serviceOrderUrl;
+
+  @RequestMapping(value = "/testRestTemplate")
+  public ResponseModel testRestTemplate(HttpServletRequest request) {
+
+    HashMap<String, Object> par = new HashMap<String, Object>();
+    // request值放到HashMap中
+    ApiUtils.setRequestPar(request, par);
+    // postRestObject参数说明
+    // 【0】服务名称 serviceOrderUrl
+    // 【1】接口地址 /order/getUserOrderInfo
+    // 【2】返回类型 ResponseModel.class
+    // 【3】参数类型 par
+    ResponseModel res = postRestObject(serviceOrderUrl, "/order/getUserOrderInfo", ResponseModel.class, par);
+    return res;
+
+  }
+
 }
